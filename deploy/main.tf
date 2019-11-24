fab0d737ba4284158f9c569849d46bb56bb1b878
OpeOnikute <opeyemionikute@yahoo.com>
2019-11-24 03:48:59 +0100

    fix: remove codecommit repository

--- deploy/main.tf
+++ deploy/main.tf
@@ -3,10 +3,3 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.aws_region}"
 }
-
-module "codecommit" {
-  source = "./modules/code-commit"
-
-  repo_name           = "${var.repo_name}"
-  repo_default_branch = "${var.repo_default_branch}"
-}
