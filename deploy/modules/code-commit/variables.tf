8a8596d7a7aa15da1dce48cdcd45c563c5394d3e
OpeOnikute <opeyemionikute@yahoo.com>
2019-11-24 03:48:19 +0100

    fix: remove codecommit repository

--- deploy/modules/code-commit/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repo_name" {
-  description = "The name of the CodeCommit repository (e.g. new-repo)."
-  default     = "example"
-}
-
-variable "repo_default_branch" {
-  description = "The name of the default repository branch (default: master)"
-  default     = "master"
-}
