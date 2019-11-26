556cd3b1c490983dffa56a87eb1133d9381dcc6f
OpeOnikute <opeyemionikute@yahoo.com>
2019-11-27 00:30:48 +0100

    feat: implement ecs monitoring with cloudwatch

--- deploy/modules/ecs/main.tf
+++ deploy/modules/ecs/main.tf
@@ -166,6 +166,11 @@ data "template_file" "web" {
 
 resource "aws_ecs_cluster" "cluster" {
   name = "${var.ecs_cluster_name}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
 
 /*
